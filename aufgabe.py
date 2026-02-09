76320d178bd40f972c0649aac4781d1b98d125c1
Rollo420 <peterhannes550@gmail.com>
2026-02-09 09:32:24 +0100

    add aufgabe to project

---
+++ aufgabe.py
@@ -0,0 +1,4 @@
+# Aufgabe: Erstelle einen Observer mit dem Namen "RAMObserver" zur Überwachung des RAMs auf dem PC.
+# Der Observer soll in der Datei logic.py implementiert werden und in der Datei aufgabe.py aufgerufen werden.
+# Anschließend soll in der Datei aufgabe.py ein Subject mit dem Namen "RAMSubject" erstellt werden, um den RAM zu überwachen.
+# Achte darauf, so wenig wie möglich die main.py-Datei zur Hilfe zu nehmen; konzentriere dich hauptsächlich auf logic.py und interfaces.py.
